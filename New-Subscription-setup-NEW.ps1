dc9f63d34190abfcf6c9c5bc8bf9ae58b34d79e1
shoaib-mohiuddin <shoaib.mohiuddin@cloudreach.com>
2024-08-28 21:51:22 +0530

    some more scripts

---
+++ New-Subscription-setup-NEW.ps1
@@ -0,0 +1,107 @@
+Param(
+    [Parameter(Mandatory = $true)]
+    [string] $SubscriptionName,
+	[Parameter(Mandatory = $true)]
+	[ValidateSet('DatacenterExtension', 'OffNetwork')]
+    [string] $SubscriptionType
+)
+
+#trim all spaces from the front and tail
+function funccullspaces($param) {
+    $param = $param.trim()
+    $param = $param.replace(' ', '')
+    return $param
+}
+
+Import-Module Az.Accounts
+Import-Module Az.Automation
+#Import-Module Az.Compute
+Import-Module Az.Resources
+Import-Module Az.Monitor
+Import-Module Az.Security
+Import-Module Az.ManagementPartner
+
+Disable-AzContextAutosave -Scope Process
+#$AzureContext = (Connect-AzAccount -Identity).context
+try
+{
+    "Logging in to Azure..."
+    Connect-AzAccount -Identity
+}
+catch {
+    Write-Error -Message $_.Exception
+    throw $_.Exception
+}
+#$AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContex
+
+$sub = Get-AzSubscription -subscriptionName $SubscriptionName
+
+Select-AzSubscription -SubscriptionId $sub.id  -TenantId $sub.TenantId
+
+### Partner Admin Link (PAL) to Eviden ID
+$PartnerId = Get-AzAutomationVariable -Name 'PartnerId'
+#$PartnerId = "6738974"
+
+$partnerInfo = Get-AzManagementPartner
+Write-Output $partnerInfo
+
+# Check if the partner ID exists
+if ($partnerInfo -and $partnerInfo.PartnerId -eq $PartnerId) {
+    Write-Output "Partner ID $PartnerId already exists."
+} 
+
+else {
+    Write-Output "Partner ID $PartnerId does not exist. Adding the partner ID..."
+    # Add the partner ID
+    New-AzManagementPartner -PartnerId $PartnerId
+    Write-Output "Partner ID $PartnerId has been added."
+}
+###
+Register-AzResourceProvider -ProviderNamespace Microsoft.ManagedServices # For Lighthouse
+Register-AzResourceProvider -ProviderNamespace Microsoft.PolicyInsights
+Register-AzResourceProvider -ProviderNamespace Microsoft.Security
+Register-AzResourceProvider -ProviderNamespace Microsoft.Compute
+Register-AzResourceProvider -ProviderNamespace Microsoft.Web
+Register-AzResourceProvider -ProviderNamespace Microsoft.Network
+Register-AzResourceProvider -ProviderNamespace Microsoft.Sql
+Register-AzResourceProvider -ProviderNamespace Microsoft.Storage
+Register-AzResourceProvider -ProviderNamespace Microsoft.insights
+Register-AzResourceProvider -ProviderNamespace Microsoft.SqlVirtualMachine
+Register-AzResourceProvider -ProviderNamespace Microsoft.EventGrid
+Register-AzProviderFeature -FeatureName BulkRegistration -ProviderNamespace Microsoft.SqlVirtualMachine
+Register-AzProviderFeature -FeatureName "EncryptionAtHost" -ProviderNamespace "Microsoft.Compute" 
+
+$currentSub = Get-AzContext
+$Scope = "/subscriptions/$($currentSub.Subscription.Id)"
+
+#SOC Activity Log Export
+Add-AzLogProfile -name "default" -ServiceBusRuleId "/subscriptions/ec057239-e4b9-4f3a-bb91-769e0d722e04/resourceGroups/RG-PROD-SOC-NE01/providers/Microsoft.EventHub/namespaces/EH-PROD-SOC-NE01/authorizationrules/RootManageSharedAccessKey"`
+-RetentionInDays 3 -Category Write, Delete, Action -Location australiacentral, australiacentral2, australiaeast, australiasoutheast, brazilsouth, canadacentral, canadaeast, centralindia, centralus, eastasia, eastus, eastus2, francecentral, francesouth, germanynorth, germanywestcentral, japaneast, japanwest, koreacentral, koreasouth, northcentralus, northeurope, southafricanorth, southafricawest, southcentralus, southindia, southeastasia, uaecentral, uaenorth, uksouth, ukwest, westcentralus, westeurope, westindia, westus, westus2, global
+
+
+$Tags = @{"SubscriptionType"="$SubscriptionType"}
+New-AzTag -ResourceId $Scope -Tag $Tags
+
+# Run this only if PALO-ALTO app registration is required [check with Andy/David]
+#$ER = [System.Windows.Forms.MessageBox]::Show("Express Route Connectivity?" , "Express Route " , 4, 64)
+#if ($SubscriptionType -eq 'DatacenterExtension') {
+
+#Palo Panorama Intergration
+#$subscription = $currentSub.Name
+#$appName = "$subscription-PaloAltoPanoramaIntergration"
+#$myApp = New-AzADServicePrincipal -Role "reader" -Scope "/subscriptions/$($scope.Subscription.Id)" -DisplayName $appName
+#$supersecure = convertfrom-securestring -SecureString $myApp.Secret
+#$object = New-Object –TypeName PSObject
+#$object | Add-Member –MemberType NoteProperty –Name Subscription –Value $subscription
+#$object | Add-Member –MemberType NoteProperty –Name DisplayName –Value $myApp.DisplayName
+#$object | Add-Member –MemberType NoteProperty –Name ApplicationId –Value $myApp.ApplicationId
+#$object | Add-Member –MemberType NoteProperty –Name Secret –Value $supersecure
+#$resultsarray += $object
+#}
+
+#$resultsarray   
+
+Start-Sleep -Seconds 600
+
+Set-AzSecurityContact -Name "default" -Email "SecurityTeamNotification@hyperiongrp.com" -AlertAdmin -NotifyOnAlert 
+#[System.Windows.MessageBox]::Show('Add SecurityTeamNotification@hyperiongrp.com to Security Center > Security policy Settings > Sub Name > Email notifications')
---
+++ TEST-configdb-update.ps1
@@ -0,0 +1,71 @@
+
+Param(
+
+    [Parameter(Mandatory = $true)]
+    [string] $SubscriptionName,
+    [Parameter(Mandatory = $true)]
+    [string] $Location,
+	[Parameter(Mandatory = $true)]
+    [string] $vnetName,
+    [Parameter(Mandatory = $true)]
+    [string] $centraldiagaccount,
+    [Parameter(Mandatory = $false)]
+    [string] $diagSA,
+    [Parameter(Mandatory = $false)]
+    [string] $prodvaultname,
+    [Parameter(Mandatory = $false)]
+    [string] $prodvaultdefaultpolname,
+    [Parameter(Mandatory = $false)]
+    [string] $nonprodvaultname,
+    [Parameter(Mandatory = $false)]
+    [string] $nonprodvaultdefaultpolname
+
+)
+
+try
+{
+    "Logging in to Azure..."
+    Connect-AzAccount -Identity
+}
+catch {
+    Write-Error -Message $_.Exception
+    throw $_.Exception
+}
+
+Import-Module Az.Accounts
+Import-Module Az.Automation
+Import-Module Az.Resources
+
+
+select-azsubscription -subscriptionname $SubscriptionName
+$sqlcreds = Get-AutomationPSCredential -Name 'sql-autoacc-test.database.windows.net'
+
+#trim all spaces from the front and tail
+function funccullspaces($param) {
+    $param = $param.trim()
+    $param = $param.replace(' ', '')
+    return $param
+}
+
+$SubscriptionName = funccullspaces ($SubscriptionName)
+$Location = funccullspaces ($Location)
+$vnetName = funccullspaces ($vnetName)
+$centraldiagaccount = funccullspaces ($centraldiagaccount)
+$diagSA = funccullspaces ($diagSA)
+$prodvaultname = funccullspaces ($prodvaultname)
+$prodvaultdefaultpolname = funccullspaces ($prodvaultdefaultpolname)
+$nonprodvaultname = funccullspaces ($nonprodvaultname)
+$nonprodvaultdefaultpolname= funccullspaces ($nonprodvaultdefaultpolname)
+
+$SubscriptionName = $SubscriptionName.ToUpper()
+#$Location = $Location.ToLower()
+
+Invoke-Sqlcmd -Query "INSERT INTO dbo.Provisioning (Subscription, Location, vnetName, centraldiagaccount, diagSA, prodvaultname, prodvaultdefaultpolname, nonprodvaultname, nonprodvaultdefaultpolname) VALUES ('$SubscriptionName', '$Location', '$vnetName', '$centraldiagaccount', '$diagSA', '$prodvaultname', '$prodvaultdefaultpolname', '$nonprodvaultname', '$nonprodvaultdefaultpolname'); " `
+    -ServerInstance "sql-autoacc-test.database.windows.net" -Credential $sqlcreds -Database "sql-autoacc-test" -QueryTimeout 20
+
+
+$configdbreturn = Invoke-Sqlcmd -Query "SELECT * From dbo.Provisioning WHERE (Location like '$Location') AND (Subscription like '$SubscriptionName'); " `
+    -ServerInstance "sql-autoacc-test.database.windows.net" -Credential $sqlcreds -Database "sql-autoacc-test" -QueryTimeout 20
+
+write-Output "Config DB request return"
+write-Output $configdbreturn
