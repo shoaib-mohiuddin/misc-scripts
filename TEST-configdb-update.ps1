2ee833bdb4d85e83fb46df8eefd76ef8a46ddbb4
shoaibmohiuddin <shoaibmohiuddin@shoaibmohiuddins-MacBook-Pro.local>
2025-07-11 21:19:55 +0530

    code blocks to update all DB tables

--- TEST-configdb-update.ps1
+++ TEST-configdb-update.ps1
@@ -1,25 +1,32 @@
-
 Param(
-
     [Parameter(Mandatory = $false)]
     [string] $SubscriptionName,
     [Parameter(Mandatory = $false)]
+    [string] $ResourceGroup,
+    [Parameter(Mandatory = $false)]
     [string] $Location,
-	[Parameter(Mandatory = $false)]
+    [Parameter(Mandatory = $false)]
     [string] $vnetName,
+    [Parameter(Mandatory = $false)]
+    [string] $vnetAddressPrefix,
     [Parameter(Mandatory = $false)]
     [string] $centraldiagaccount,
     [Parameter(Mandatory = $false)]
     [string] $diagSA,
+    # [Parameter(Mandatory = $false)]
+    # [string] $prodvaultname,
+    # [Parameter(Mandatory = $false)]
+    # [string] $prodvaultdefaultpolname,
+    # [Parameter(Mandatory = $false)]
+    # [string] $nonprodvaultname,
+    # [Parameter(Mandatory = $false)]
+    # [string] $nonprodvaultdefaultpolname,
     [Parameter(Mandatory = $false)]
-    [string] $prodvaultname,
+    [string] $nextHopType,
     [Parameter(Mandatory = $false)]
-    [string] $prodvaultdefaultpolname,
+    [string] $hubName,
     [Parameter(Mandatory = $false)]
-    [string] $nonprodvaultname,
-    [Parameter(Mandatory = $false)]
-    [string] $nonprodvaultdefaultpolname
-
+    [string] $nextHopIP
 )
 
 try
@@ -37,7 +44,7 @@ Import-Module Az.Automation
 Import-Module Az.Resources
 
 
-select-azsubscription -subscriptionname $SubscriptionName
+Select-AzSubscription -subscriptionname $SubscriptionName
 $sqlcreds = Get-AutomationPSCredential -Name 'sql-autoacc-test.database.windows.net' 
 
 #trim all spaces from the front and tail
@@ -52,20 +59,54 @@ $Location = funccullspaces ($Location)
 $vnetName = funccullspaces ($vnetName)
 $centraldiagaccount = funccullspaces ($centraldiagaccount)
 $diagSA = funccullspaces ($diagSA)
-$prodvaultname = funccullspaces ($prodvaultname)
-$prodvaultdefaultpolname = funccullspaces ($prodvaultdefaultpolname)
-$nonprodvaultname = funccullspaces ($nonprodvaultname)
-$nonprodvaultdefaultpolname= funccullspaces ($nonprodvaultdefaultpolname)
+$vnetAddressPrefix = funccullspaces ($vnetAddressPrefix)
+$nextHopType = funccullspaces ($nextHopType)
+$hubName = funccullspaces ($hubName)
+$nextHopIP = funccullspaces ($nextHopIP)
+$prodvaultname = 'commvault'
+$prodvaultdefaultpolname = 'commvault'
+$nonprodvaultname = 'commvault'
+$nonprodvaultdefaultpolname= 'commvault'
 
 $SubscriptionName = $SubscriptionName.ToUpper()
-#$Location = $Location.ToLower()
+$ResourceGroup = $ResourceGroup.ToUpper()
+$vnetName = $vnetName.ToUpper()
+$hubName = $hubName.ToUpper()
+$Location = $Location.ToLower()
 
-Invoke-Sqlcmd -Query "INSERT INTO dbo.Provisioning (Subscription, Location, vnetName, centraldiagaccount, diagSA, prodvaultname, prodvaultdefaultpolname, nonprodvaultname, nonprodvaultdefaultpolname) VALUES ('$SubscriptionName', '$Location', '$vnetName', '$centraldiagaccount', '$diagSA', '$prodvaultname', '$prodvaultdefaultpolname', '$nonprodvaultname', '$nonprodvaultdefaultpolname'); " `
+# UPDATE LOCKS DB TABLE
+if ($ResourceGroup -and $SubscriptionName) {
+    Invoke-Sqlcmd -Query "INSERT INTO dbo.Locks (ReourceGroupName, SubscriptionName) VALUES ('$ResourceGroup', '$SubscriptionName'); " `
+        -ServerInstance "sql-autoacc-test.database.windows.net" -Credential $sqlcreds -Database "sql-autoacc-test" -QueryTimeout 20
+}
+$lockdbreturn = Invoke-Sqlcmd -Query "SELECT * From dbo.Locks WHERE SubscriptionName='$SubscriptionName'; " `
     -ServerInstance "sql-autoacc-test.database.windows.net" -Credential $sqlcreds -Database "sql-autoacc-test" -QueryTimeout 20
 
+Write-Output "Lock DB request return"
+Write-Output $lockdbreturn
 
-$configdbreturn = Invoke-Sqlcmd -Query "SELECT * From dbo.Provisioning WHERE Subscription='$SubscriptionName'; " `
+# UPDATE PROVISIONING DB TABLE
+if ($vnetName -and $Location -and $centraldiagaccount -and $diagSA ) {
+    Invoke-Sqlcmd -Query "INSERT INTO dbo.Provisioning (Subscription, Location, vnetName, centraldiagaccount, diagSA, prodvaultname, prodvaultdefaultpolname, nonprodvaultname, nonprodvaultdefaultpolname) VALUES ('$SubscriptionName', '$Location', '$vnetName', '$centraldiagaccount', '$diagSA', '$prodvaultname', '$prodvaultdefaultpolname', '$nonprodvaultname', '$nonprodvaultdefaultpolname'); " `
+        -ServerInstance "sql-autoacc-test.database.windows.net" -Credential $sqlcreds -Database "sql-autoacc-test" -QueryTimeout 20
+}
+$provisioningdbreturn = Invoke-Sqlcmd -Query "SELECT * From dbo.Provisioning WHERE Subscription='$SubscriptionName'; " `
     -ServerInstance "sql-autoacc-test.database.windows.net" -Credential $sqlcreds -Database "sql-autoacc-test" -QueryTimeout 20
 
-write-Output "Config DB request return"
-write-Output $configdbreturn
+Write-Output "Provisioning DB request return"
+Write-Output $provisioningdbreturn
+
+# UPDATE ROUTES DB TABLE
+if ($vnetName -and $vnetAddressPrefix -and $nextHopType -and $hubName -and $nextHopIP -and $Location -in @("westeurope", "northeurope", "uksouth")) {
+    Invoke-Sqlcmd -Query "INSERT INTO dbo.Routes (Name, AddressPrefix, NextHopType, HubName, NetxtHopIP, Region) VALUES ('$vnetName', '$vnetAddressPrefix', '$nextHopType', '$hubName', '$nextHopIP', 'all'); " `
+        -ServerInstance "sql-autoacc-test.database.windows.net" -Credential $sqlcreds -Database "sql-autoacc-test" -QueryTimeout 20
+}
+if ($vnetName -and $vnetAddressPrefix -and $nextHopType -and $hubName -and $nextHopIP -and $Location -notin @("westeurope", "northeurope", "uksouth")) {
+    Invoke-Sqlcmd -Query "INSERT INTO dbo.Routes (Name, AddressPrefix, NextHopType, HubName, NetxtHopIP, Region) VALUES ('$vnetName', '$vnetAddressPrefix', '$nextHopType', '$hubName', '$nextHopIP', '$Location'); " `
+        -ServerInstance "sql-autoacc-test.database.windows.net" -Credential $sqlcreds -Database "sql-autoacc-test" -QueryTimeout 20
+}
+$routesdbreturn = Invoke-Sqlcmd -Query "SELECT * From dbo.Routes WHERE Name='$vnetName'; " `
+    -ServerInstance "sql-autoacc-test.database.windows.net" -Credential $sqlcreds -Database "sql-autoacc-test" -QueryTimeout 20
+
+Write-Output "Routes DB request return"
+Write-Output $routesdbreturn
